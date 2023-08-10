c9e59640acaad4cbb87b9e4fd740111b2315a14f
Frankenstein9585 <donny9585@gmail.com>
2023-08-10 19:19:52 +0100

    Modified task 6 again

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,13 +2,13 @@
 DELIMITER $$
 CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-	IF NOT EXISTS (SELECT FROM projects WHERE name = project_name) THEN
+	IF NOT EXISTS (SELECT * FROM projects WHERE name = project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
 	DECLARE project_id INT;
 	SET project_id = (SELECT id FROM projects WHERE name = project_name);
 	UPDATE corrections
-	SET corrections.score = score 
+	SET score = score 
 	WHERE user_id = user_id AND project_id = project_id;
 
 END$$
