67392d5defab8425c10fdca2ed8944286d3c2037
Frankenstein9585 <donny9585@gmail.com>
2023-08-15 23:50:31 +0100

    Changed function name

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,7 +4,7 @@ new document in a collection based on kwargs"""
 import pymongo
 
 
-def list_all(mongo_collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """This function inserts a
     new document in a collection based on kwargs"""
     result = mongo_collection.insert_one(kwargs)
