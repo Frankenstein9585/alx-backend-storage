3ccf6679949496ce7b98fa31b5131ebaac64ff67
Frankenstein9585 <donny9585@gmail.com>
2023-08-15 23:47:42 +0100

    Done task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This file contains a function inserts a
+new document in a collection based on kwargs"""
+import pymongo
+
+
+def list_all(mongo_collection, **kwargs):
+    """This function inserts a
+    new document in a collection based on kwargs"""
+    result = mongo_collection.insertOne(kwargs)
+    return result.inserted_id
