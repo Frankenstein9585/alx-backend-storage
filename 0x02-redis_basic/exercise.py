ea8a2b0dafef5f2c6c710fa2d73b4f8ba433e5a7
Frankenstein9585 <donny9585@gmail.com>
2023-08-16 19:52:59 +0100

    Modified task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """This file contains a class that handle data storage using redis"""
+from typing import Union
+
 import redis
 import uuid
 
@@ -11,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """This method takes data as an argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
